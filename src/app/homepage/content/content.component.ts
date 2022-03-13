72910ac11a12cf50b57e8cca86ea4c6604b51bae
GersonMurguia <aldmurguia1@gmail.com>
2022-03-12 23:53:10 -0500

    agregado servicio de url

--- src/app/homepage/content/content.component.ts
+++ src/app/homepage/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UrlService } from '../../service/url.service';
 
 @Component({
   selector: 'app-content',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+  
+  public refreshing: boolean=false;
 
-  constructor() { }
+  constructor(private urlService:UrlService) { }
 
   ngOnInit(): void {
+    //obtener urls del local cache
   }
 
+  public createShortUrl():void {
+    this.refreshing=true;
+  }
 }
